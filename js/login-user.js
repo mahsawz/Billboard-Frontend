ce4450dfb54709838eaa0a6b4bf119a9e52fcfe1
Arash <arash_moshirnia@yahoo.com>
2018-11-30 22:10:57 +0330

    feat: Implement user signup with API (signupUser function)

--- js/login-user.js
+++ js/login-user.js
@@ -13,14 +13,46 @@ function verifyUser() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       var resp = JSON.parse(this.response);
       if (resp.status === "OK") {
-        sessionStorage.setItem('user',resp.user);
+        sessionStorage.setItem('user', resp.user);
         window.location.replace('app.html');
-      }
-      else {
+      } else {
         alert("User info not correct");
       }
+    } else {
+      alert("Request not made");
     }
-    else {
+  }
+  request.send(JSON.stringify(user));
+}
+
+function signupUser() {
+  var request = new XMLHttpRequest();
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('pass').value;
+  var passrepeat = document.getElementById('passrpt').value;
+  if (password != passrepeat) {
+    alert("Please repeat password correctly");
+    return;
+  }
+  var user = {
+    'name': name,
+    'email': email,
+    'password': password
+  };
+  var url = 'http://127.0.0.1:5000/api/signup';
+  request.open('POST', url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onload = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      var resp = JSON.parse(this.response);
+      if (resp.status == "OK") {
+        alert("Signup Successful");
+      }
+      else {
+        alert("Signup Unsuccessful");
+      }
+    } else {
       alert("Request not made");
     }
   }
