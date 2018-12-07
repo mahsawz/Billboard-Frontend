190a387e6d9e4caf6693267926f4ad602535b0cb
Arash <arash_moshirnia@yahoo.com>
2018-12-07 20:02:27 +0330

    feat: Fetch and show updated user data after gift redeem/survey answer

--- js/fill-userInfo.js
+++ js/fill-userInfo.js
@@ -3,3 +3,25 @@ function fillUserInfo(user) {
   document.getElementById('user-name').innerHTML = user.name;
   document.getElementById('user-credit').innerHTML = "اعتبار : "+user.credit;
 }
+
+
+function getUpdatedData() {
+  var user = JSON.parse(sessionStorage.getItem('user'));
+  var request = new XMLHttpRequest();
+  var url = 'http://127.0.0.1:5000/api/getUser/'+user.id;
+  request.open('GET', url, true);
+  request.onload = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      var resp = JSON.parse(this.response);
+      if (resp.status === "OK") {
+        var updatedUser = resp.user;
+        sessionStorage.removeItem("user");
+        sessionStorage.setItem('user', JSON.stringify(updatedUser));
+        fillUserInfo();
+      } else {
+        alert("Unsuccessful");
+      }
+    }
+  }
+  request.send();
+}
