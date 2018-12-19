edf4b2f795c5d850d6a5abbfb33a1854c2089a37
Arash <arash_moshirnia@yahoo.com>
2018-12-20 00:31:25 +0330

    feat: Implement user logout script

---
+++ js/logout-user.js
@@ -0,0 +1,19 @@
+function logoutUser() {
+  var request = new XMLHttpRequest();
+  var url = 'http://192.168.1.4:5000/api/logout';
+  request.crossDomain = true;
+  request.withCredentials = true;
+  request.open('GET', url, true);
+  request.onload = function(e) {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      var resp = JSON.parse(this.response);
+      if (resp.status === "OK") {
+        alert("Logout Successful");
+        window.location.replace('index.html');
+      } else {
+        alert("Unsuccessful");
+      }
+    }
+  }
+  request.send();
+}
